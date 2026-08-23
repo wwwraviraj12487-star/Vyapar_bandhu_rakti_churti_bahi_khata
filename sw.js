83d3041078ef4d1ca6accd72a84261a688b946e8
wwwraviraj12487-star <wwwraviraj12487@gmail.com>
2026-08-23 14:38:06 +0530

    Update service worker cache name and event listeners

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'vyapar-bandhu-FINAL1001';
+const CACHE_NAME = 'vyapar-bandhu-FINAL2000';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(x => caches.delete(x)))));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request, {cache: 'no-store'}).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request, {cache: 'no-store'}).catch(()=>caches.match(e.request)));
 });
