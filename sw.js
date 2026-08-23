568c8677b69a7a85630e4da21280641b45e1f199
wwwraviraj12487-star <wwwraviraj12487@gmail.com>
2026-08-23 14:49:46 +0530

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE_NAME = 'vyapar-bandhu-FINAL2000';
-self.addEventListener('install', e => { self.skipWaiting(); });
+const CACHE_NAME = 'vyapar-bandhu-DIRECTFINAL';
+self.addEventListener('install', e => { 
+  self.skipWaiting(); 
+});
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(k => Promise.all(k.map(x => caches.delete(x)))));
+  e.waitUntil(
+    caches.keys().then(k => Promise.all(k.map(x => caches.delete(x))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request, {cache: 'no-store'}).catch(()=>caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request, {cache: 'no-store'}).catch(()=>caches.match(e.request))
+  );
 });
